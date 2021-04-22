336c91f98fa180551e3b7460666a7009ba0957b3
gnaqe <80735841+gnaqe@users.noreply.github.com>
2021-04-21 19:17:40 -0700

    No serial communications. For Teensy

--- Finished/Teensy_FlightLOGR/Teensy_FlightLOGR_NOSRL.ino
+++ Finished/Teensy_FlightLOGR/Teensy_FlightLOGR_NOSRL.ino
@@ -1,23 +1,4 @@
-/**************************************************************************/
-/*!
-    @file     Adafruit_MPL3115A2.cpp
-    @author   K.Townsend (Adafruit Industries)
-    @license  BSD (see license.txt)
 
-    Example for the MPL3115A2 barometric pressure sensor
-
-    This is a library for the Adafruit MPL3115A2 breakout
-    ----> https://www.adafruit.com/products/1893
-
-    Adafruit invests time and resources providing this open source code,
-    please support Adafruit and open-source hardware by purchasing
-    products from Adafruit!
-
-    @section  HISTORY
-
-    v1.0 - First release
-*/
-/**************************************************************************/
 #include <SPI.h>
 #include <SD.h>
 #include <Wire.h>
