500e983b2aeed4660083fc57907c226adc406870
gnaqe <80735841+gnaqe@users.noreply.github.com>
2021-04-21 19:16:56 -0700

    Arduino code for Teensy 4.1

---
+++ Finished/Teensy_FlightLOGR/Teensy_FlightLOGR_NOSRL.ino
@@ -0,0 +1,93 @@
+/**************************************************************************/
+/*!
+    @file     Adafruit_MPL3115A2.cpp
+    @author   K.Townsend (Adafruit Industries)
+    @license  BSD (see license.txt)
+
+    Example for the MPL3115A2 barometric pressure sensor
+
+    This is a library for the Adafruit MPL3115A2 breakout
+    ----> https://www.adafruit.com/products/1893
+
+    Adafruit invests time and resources providing this open source code,
+    please support Adafruit and open-source hardware by purchasing
+    products from Adafruit!
+
+    @section  HISTORY
+
+    v1.0 - First release
+*/
+/**************************************************************************/
+#include <SPI.h>
+#include <SD.h>
+#include <Wire.h>
+#include <Adafruit_MPL3115A2.h>
+
+Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
+
+
+long t0 ; 
+long tf ;
+long tdiff ; 
+long tc ; 
+
+
+const int chipSelect = BUILTIN_SDCARD;
+File flightlog ;
+
+
+void setup() {
+  
+ 
+    Serial.print("Initializing SD card...");
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    digitalWrite(13,LOW);
+    delay(100) ;
+    digitalWrite(13,HIGH);
+     delay(100) ;
+    // don't do anything more:
+    return;
+  }
+  
+  pinMode(13,OUTPUT) ;
+  
+  SD.begin(chipSelect) ; 
+}
+
+void loop() {
+  
+
+  float pascals = baro.getPressure();
+
+
+
+  
+flightlog = SD.open("flyLOGR.txt",FILE_WRITE);
+
+
+// #13 board led flash with absent SD card
+
+
+tc = millis() ;
+if (flightlog){
+  // #13 LED establishes data being written
+  digitalWrite(13,HIGH);
+
+  t0 =  millis();
+  float altm = baro.getAltitude();
+
+
+  float tempC = baro.getTemperature();
+
+  tf = millis() ; 
+  tdiff = tf - t0 ;
+
+  // Time(s) | Inches (Hg) | Altitude (meters) | Temperature (C) | Execute time
+flightlog.print(tc*.001); flightlog.print(" , "); flightlog.print(pascals/3377);flightlog.print(" , ");flightlog.print(altm);flightlog.print(" , ");flightlog.print(tempC);flightlog.print(" , ");flightlog.println(tdiff);
+flightlog.close();
+
+
+}
+}
